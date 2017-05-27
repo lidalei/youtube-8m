1b36b953c3db44ff04675e593c2c36e1998ded60
Dalei Li <lidalei151781@gmail.com>
2017-05-27 23:45:23 +0200

    Added multi-layer perceptron.

--- mlp.py
+++ mlp.py
@@ -23,6 +23,42 @@ NUM_VALIDATE_EXAMPLES = None
 NUM_TEST_EXAMPLES = 700640
 
 
+def create_hidden_layer(data, name, pre_size, size, pos_activation, pos_transform=None):
+    """
+    Create a hidden fully connected layer.
+    
+    Args:
+        data: Data output from previous layer. A tf tensor.
+        name: Layer name.
+        pre_size: Previous layer size, namely number of neurons.
+        size: Number of neurons.
+        pos_activation: Activation function.
+        pos_transform: Other transform, such as dropout and batch normalization.
+    Returns:
+        Transformed data, i.e., data after passing this layer. A tensorflow tensor.
+    """
+    with tf.name_scope(name):
+        weights = tf.Variable(initial_value=tf.truncated_normal([pre_size, size], name='weights'))
+        biases = tf.Variable(initial_value=tf.zeros([size]), name='biases')
+
+        inner_product = tf.matmul(data, weights) + biases
+        activation = pos_activation(inner_product)
+
+        # tf.GraphKeys.REGULARIZATION_LOSSES contains all variables to regularize.
+        tf.add_to_collection(tf.GraphKeys.REGULARIZATION_LOSSES, weights)
+
+        # Add to summary.
+        tf.summary.histogram('weights', weights)
+        tf.summary.histogram('biases', biases)
+        tf.summary.histogram('activation', activation)
+
+        if pos_transform is not None:
+            transformed_data = pos_transform(activation)
+            return transformed_data
+        else:
+            return activation
+
+
 def multi_layer_transform(data, mean=None, variance=None, **kwargs):
     """
     Multi-layer Perceptron transform data, incl. standard scale data using given mean and var.
@@ -35,46 +71,68 @@ def multi_layer_transform(data, mean=None, variance=None, **kwargs):
     Returns:
         transformed data. A tensorflow tensor.
     """
-    with tf.name_scope('standard_scale'):
-        features_mean = tf.Variable(initial_value=mean, trainable=False, name='features_mean')
-        features_var = tf.Variable(initial_value=variance, trainable=False, name='features_var')
-        standardized_data = tf.nn.batch_normalization(data,
-                                                      mean=features_mean, variance=features_var,
-                                                      offset=None, scale=None, variance_epsilon=1e-12,
-                                                      name='standardized')
-        # Add layers.
+    with tf.name_scope('mlp_transform'):
+        with tf.name_scope('standard_scale'):
+            features_mean = tf.Variable(initial_value=mean, trainable=False, name='features_mean')
+            features_var = tf.Variable(initial_value=variance, trainable=False, name='features_var')
+            standardized = tf.nn.batch_normalization(data, mean=features_mean, variance=features_var,
+                                                     offset=None, scale=None, variance_epsilon=1e-12,
+                                                     name='standardized')
+
+        current_size = mean.shape[-1]
+        current_data = standardized
+
+        # First Hidden layers---#
+        layer_idx = 1
+        layer_name = 'hidden_{}'.format(layer_idx)
+        layer_size = 64
+        hidden_activation = create_hidden_layer(current_data, layer_name, current_size, layer_size, tf.nn.tanh)
+        # ---First Hidden layers#
+        current_size = layer_size
+        current_data = hidden_activation
+        """
+        # Second Hidden layers---#
+        layer_idx = 2
+        layer_name = 'hidden_{}'.format(layer_idx)
+        layer_size = 32
+        hidden_activation = create_hidden_layer(current_data, layer_name, current_size, layer_size, tf.tanh)
+        # ---Second Hidden layers#
+        current_size = layer_size
+        current_data = hidden_activation
+        """
+
+        return current_data
 
 
-
-        return standardized_data
-
-
-def train(init_learning_rate, decay_steps, decay_rate=0.95, l2_reg_rate=0.01, epochs=None):
+def main(unused_argv):
     """
-    Training.
-
-    Args:
         init_learning_rate: Initial learning rate.
         decay_steps: How many training steps to decay learning rate once.
         decay_rate: How much to decay learning rate.
         l2_reg_rate: l2 regularization rate.
-        epochs: The maximal epochs to pass all training data.
-
-    Returns:
-
+        epochs: The maximal epochs to pass all training data. 
     """
+    logging.set_verbosity(logging.INFO)
+
+    start_new_model = FLAGS.start_new_model
     output_dir = FLAGS.output_dir
+
+    init_learning_rate = FLAGS.init_learning_rate
+    decay_steps = FLAGS.decay_steps
+    decay_rate = FLAGS.decay_rate
+    l2_reg_rate = FLAGS.l2_reg_rate
+    train_epochs = FLAGS.train_epochs
+
     model_type, feature_names, feature_sizes = FLAGS.model_type, FLAGS.feature_names, FLAGS.feature_sizes
     reader = get_reader(model_type, feature_names, feature_sizes)
     train_data_pattern = FLAGS.train_data_pattern
     validate_data_pattern = FLAGS.validate_data_pattern
     batch_size = FLAGS.batch_size
     num_readers = FLAGS.num_readers
-    is_bootstrap = FLAGS.is_bootstrap
 
     # Increase num_readers.
     validate_data_pipeline = DataPipeline(reader=reader, data_pattern=validate_data_pattern,
-                                          batch_size=batch_size, num_readers=2 * num_readers)
+                                          batch_size=batch_size, num_readers=num_readers)
 
     # Sample validate set for line search in linear classifier or logistic regression early stopping.
     _, validate_data, validate_labels, _ = random_sample(0.05, mask=(False, True, True, False),
@@ -83,33 +141,28 @@ def train(init_learning_rate, decay_steps, decay_rate=0.95, l2_reg_rate=0.01, ep
     train_data_pipeline = DataPipeline(reader=reader, data_pattern=train_data_pattern,
                                        batch_size=batch_size, num_readers=num_readers)
 
-    # Load train data mean and std.
-    train_features_mean, train_features_var = load_features_mean_var(reader)
+    # If start a new model or output dir does not exist, truly start a new model.
+    start_new_model = start_new_model or (not tf.gfile.Exists(output_dir))
 
-    tr_data_fn = standard_scale
-    tr_data_paras = {'mean': train_features_mean, 'variance': train_features_var,
-                     'reshape': False, 'size': None}
+    if start_new_model:
+        # Load train data mean and std.
+        train_features_mean, train_features_var = load_features_mean_var(reader)
+
+        tr_data_fn = multi_layer_transform
+        tr_data_paras = {'mean': train_features_mean, 'variance': train_features_var,
+                         'reshape': True, 'size': 64}
+    else:
+        tr_data_fn = None
+        tr_data_paras = dict()
 
     # Run logistic regression.
-    log_reg = LogisticRegression(logdir=path_join(output_dir, 'log_reg'))
-    log_reg.fit(train_data_pipeline,
+    log_reg = LogisticRegression(logdir=output_dir)
+    log_reg.fit(train_data_pipeline, start_new_model=start_new_model,
                 tr_data_fn=tr_data_fn, tr_data_paras=tr_data_paras,
-                validate_set=(validate_data, validate_labels), validate_fn=gap_fn, bootstrap=is_bootstrap,
+                validate_set=(validate_data, validate_labels), validate_fn=gap_fn, bootstrap=False,
                 init_learning_rate=init_learning_rate, decay_steps=decay_steps, decay_rate=decay_rate,
-                epochs=epochs, l2_reg_rate=l2_reg_rate, pos_weights=None,
+                epochs=train_epochs, l1_reg_rate=None, l2_reg_rate=l2_reg_rate, pos_weights=None,
                 initial_weights=None, initial_biases=None)
-
-
-def main(unused_argv):
-    logging.set_verbosity(logging.INFO)
-
-    init_learning_rate = FLAGS.init_learning_rate
-    decay_steps = FLAGS.decay_steps
-    decay_rate = FLAGS.decay_rate
-    l2_reg_rate = FLAGS.l2_reg_rate
-    train_epochs = FLAGS.train_epochs
-
-    train(init_learning_rate, decay_steps, decay_rate=decay_rate, l2_reg_rate=l2_reg_rate, epochs=train_epochs)
 
 
 if __name__ == '__main__':
@@ -133,19 +186,21 @@ if __name__ == '__main__':
     flags.DEFINE_integer('batch_size', 1024, 'Size of batch processing.')
     flags.DEFINE_integer('num_readers', 2, 'Number of readers to form a batch.')
 
-    flags.DEFINE_float('init_learning_rate', 0.01, 'Float variable to indicate initial learning rate.')
+    flags.DEFINE_bool('start_new_model', True, 'To start a new model or restore from output dir.')
+
+    flags.DEFINE_float('init_learning_rate', 0.001, 'Float variable to indicate initial learning rate.')
 
     flags.DEFINE_integer('decay_steps', NUM_TRAIN_EXAMPLES,
                          'Float variable indicating no. of examples to decay learning rate once.')
 
     flags.DEFINE_float('decay_rate', 0.95, 'Float variable indicating how much to decay.')
 
-    flags.DEFINE_float('l2_reg_rate', 0.01, 'l2 regularization rate.')
+    flags.DEFINE_float('l2_reg_rate', 0.1, 'l2 regularization rate.')
 
     flags.DEFINE_integer('train_epochs', 200, 'Training epochs, one epoch means passing all training data once.')
 
     # Added current timestamp.
-    flags.DEFINE_string('output_dir', '/tmp/video_level',
+    flags.DEFINE_string('output_dir', '/tmp/video_level/mlp',
                         'The directory where intermediate and model checkpoints should be written.')
 
     app.run()
