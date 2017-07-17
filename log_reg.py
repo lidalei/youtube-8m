6750cb136baa19271a3e1294e94d0b693e1f907e
Dalei Li <lidalei151781@gmail.com>
2017-07-17 19:49:24 +0200

    Reuse validation set to have comparable results.

--- log_reg.py
+++ log_reg.py
@@ -19,6 +19,7 @@ from utils import gap_fn
 from linear_model import LinearClassifier, LogisticRegression
 
 from os.path import join as path_join
+import pickle
 
 
 FLAGS = flags.FLAGS
@@ -83,9 +84,22 @@ def main(unused_argv):
     validate_data_pipeline = DataPipeline(reader=reader, data_pattern=validate_data_pattern,
                                           batch_size=batch_size, num_readers=num_readers)
 
-    # Sample validate set for line search in linear classifier or logistic regression early stopping.
-    _, validate_data, validate_labels, _ = random_sample(0.05, mask=(False, True, True, False),
-                                                         data_pipeline=validate_data_pipeline)
+    if tf.gfile.Exists(path_join(output_dir, 'validate_data.pickle')):
+        with open(path_join(output_dir, 'validate_data.pickle', 'rb')) as f:
+            validate_data = pickle.load(f)
+
+        with open(path_join(output_dir, 'validate_labels.pickle', 'rb')) as f:
+            validate_labels = pickle.load(f)
+    else:
+        # Sample validate set for line search in linear classifier or logistic regression early stopping.
+        _, validate_data, validate_labels, _ = random_sample(0.05, mask=(False, True, True, False),
+                                                             data_pipeline=validate_data_pipeline)
+        with open(path_join(output_dir, 'validate_data.pickle', 'wb')) as f:
+            pickle.dump(validate_data, f)
+
+        with open(path_join(output_dir, 'validate_labels.pickle', 'wb')) as f:
+            pickle.dump(validate_labels, f)
+
     start_new_model = start_new_model or (not tf.gfile.Exists(output_dir))
 
     # Set pos_weights for extremely imbalanced situation in one-vs-all classifiers.
