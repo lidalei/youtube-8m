478fd80ae6efe6386ea2b2e10af940563c21a756
Dalei Li <lidalei151781@gmail.com>
2017-09-29 10:00:15 +0200

    Migrated to Python 3.

--- mlp_fuse_rgb_audio.py
+++ mlp_fuse_rgb_audio.py
@@ -356,7 +356,8 @@ def train(train_data_pipeline, epochs=None, pos_weights=None, l1_reg_rate=None, 
 
                     # Compute validation loss.
                     num_validate_videos = validate_data.shape[0]
-                    split_indices = np.linspace(0, num_validate_videos, num_validate_videos / (4 * batch_size),
+                    split_indices = np.linspace(0, num_validate_videos,
+                                                max(num_validate_videos // (2 * batch_size), 2),
                                                 dtype=np.int32)
 
                     validate_loss_vals, predictions = [], []
