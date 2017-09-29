Fixed validation performance computation bug.

linear_model.py
@@ -222,7 +222,8 @@ class LinearClassifier(object):
             weights_val, biases_val = sess.run([weights, biases], feed_dict={l2_reg_ph: l2_reg})
             # Compute validation loss.
             num_validate_videos = validate_data.shape[0]
-            split_indices = np.linspace(0, num_validate_videos, num_validate_videos / batch_size,
+            split_indices = np.linspace(0, num_validate_videos + 1,
+                                        num=max(num_validate_videos // batch_size, 2),
                                         dtype=np.int32)
             loss_vals = []
             for i in range(len(split_indices) - 1):
@@ -620,7 +621,7 @@ class LogisticRegression(object):
 
                         # Compute validation loss.
                         num_validate_videos = validate_data.shape[0]
-                        split_indices = np.linspace(0, num_validate_videos, max(
+                        split_indices = np.linspace(0, num_validate_videos + 1, max(
                             num_validate_videos // (2 * batch_size), 2), dtype=np.int32)
 
                         validate_loss_vals, predictions = [], []

ml-knn.py
@@ -357,7 +357,8 @@ def compute_prior_posterior_prob(k_list=[8], smooth_para=1.0, opt_hyper_para=Fal
         for k in k_list:
             pred_obj = Predict(train_data_pipeline, model_dir, k=k)
             num_validate_videos = validate_data.shape[0]
-            split_indices = np.linspace(0, num_validate_videos, num_validate_videos / batch_size, dtype=np.int32)
+            split_indices = np.linspace(0, num_validate_videos + 1,
+                                        max(num_validate_videos // batch_size, 2), dtype=np.int32)
 
             validate_gaps = []
             for i in range(len(split_indices) - 1):

mlp_fuse_rgb_audio.py
@@ -356,7 +356,7 @@ def train(train_data_pipeline, epochs=None, pos_weights=None, l1_reg_rate=None, 
 
                     # Compute validation loss.
                     num_validate_videos = validate_data.shape[0]
-                    split_indices = np.linspace(0, num_validate_videos,
+                    split_indices = np.linspace(0, num_validate_videos + 1,
                                                 max(num_validate_videos // (2 * batch_size), 2),
                                                 dtype=np.int32)
 